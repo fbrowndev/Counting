e3ed08d071379dcb8b55409ffb7d7809f37e5bb0
Keke Brown <fbrowndev@gmail.com>
2022-10-26 14:09:26 -0400

    Player Movement Complete
    Player able to move left and right on the z axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Handling all movement of the box that will be used to catch objects
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
+    #region Controller Variables
+    [Header("Player Speeds")]
+    [SerializeField] float speed = 10f;
+
+    float horizontalMovement;
+
+    #endregion
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HorizontalMovement();
     }
+
+
+    #region Basic Movement
+    void HorizontalMovement()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * horizontalMovement * speed * Time.deltaTime);
+    }
+
+    #endregion
 }
